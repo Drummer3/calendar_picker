2c058b9cff28e9b87b75acde1824e9aef53f78f0
akvin18 <drummerthree@gmail.com>
2022-09-08 12:45:49 +0400

    tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
+		"./styles/**/*.{css,scss}",
 	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/forms")],
 };
